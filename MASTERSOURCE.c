67128ef18defb86f221f397e9b65e94b5cb745bc
DLSUAdrielAmoguis <60635460+DLSUAdrielAmoguis@users.noreply.github.com>
2020-03-24 01:44:57 +0800

    Update MASTERSOURCE.c
    Initially created the base display grid function. A problem we need to tackle (as I have realized) is trying to create a coherent grid whereas even the distance between two nodes are shown. My initial idea is just to output the grid to a text file, but we'll discuss that. Using the same logic that Kyle has created initially, I converted it into a structured type for easier handling.

--- MASTERSOURCE.c
+++ MASTERSOURCE.c
@@ -8,7 +8,7 @@
 	Group Project
 	
 	Source Code Created On: March 15, 2020
-	Source Code Last Edited On: March 15, 2020
+	Source Code Last Edited On: March 24, 2020
 	
 	SOURCE NOTES:
 		- [Adi - 03/15/2020] Let's get this rollin'!
@@ -18,72 +18,160 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <string.h>
 
 // Header Imports
 
 
 // Macro Definitions
+#define NODENAME_LEN 31
+#define MAX_MUNICIPAL 5
+#define MAX_CITIES 7
+#define MAX_NODES (MAX_MUNICIPAL * MAX_CITIES)
 
 
+// Structure Definitions
+struct spNeighborTag {
+	int top;
+	int right;
+	int bottom;
+	int left;
+};
 
 // Type Definitions
-
+typedef struct spNodeTag {
+	char name[NODENAME_LEN];
+	char cityName[NODENAME_LEN];
+	struct spNeighborTag distance;
+} spNode;
 
 // Functions
-int main() {
-	char* cCities[7] = {"Muntinlupa","Taguig","Pasig","Mandaluyong","Manila","Quezon","Caloocan"};
-	char* cMunicipality[5][7];
+/* This function initializes all values of the node array for shortest path.
+	This function is called by the setShortestPathNodes function. */
+void initShortestPathNodes(spNode nodes[][MAX_CITIES]) {
+	int i, j;
+	
+	for(i=0; i<MAX_CITIES; i++) {
+		for(j=0; j<MAX_MUNICIPAL; j++) {
+			strcpy(nodes[j][i].name, "NULL");
+			strcpy(nodes[j][i].cityName, "NULL");
+			nodes[j][i].distance.top = -1;
+			nodes[j][i].distance.right = -1;
+			nodes[j][i].distance.bottom = -1;
+			nodes[j][i].distance.left = -1;
+		}
+	}
+}
 
-	cMunicipality[0][0] = "New Bilibid";
-	cMunicipality[1][0] = "Starmall Alabang";
-	cMunicipality[2][0] = "Yexel's Toy Museum";
-	cMunicipality[3][0] = "Jamboree Lake";
-	cMunicipality[4][0] = "Sports Complex";
+void setShortestPathNodes(spNode nodes[][MAX_CITIES]) {
+	// Initialize Node List
+	initShortestPathNodes(nodes);
 	
-	cMunicipality[0][1] = "SM Aura";
-	cMunicipality[1][1] = "BGC";
-	cMunicipality[2][1] = "Mind Museum";
-	cMunicipality[3][1] = "Enderun University";
-	cMunicipality[4][1] = "Manila American Cemetery";
+	// Variable Declarations
+	int i;
 	
-	cMunicipality[0][2] = "Hampton Gardens";
-	cMunicipality[1][2] = "St Paul College";
-	cMunicipality[2][2] = "Estancia";
-	cMunicipality[3][2] = "Medical City";
-	cMunicipality[4][2] = "C5 Road";
+	// Set Node Values
+	// MUNTINLUPA
+	strcpy(nodes[0][0].name, "New Bilibid Prison Cemetery");
+	strcpy(nodes[1][0].name, "Starmall Alabang");
+	strcpy(nodes[2][0].name, "Yexel's Toy Museum");
+	strcpy(nodes[3][0].name, "Jamboree Lake");
+	strcpy(nodes[4][0].name, "Muntinlupa Sports Complex");
+		// Set city
+	for(i=0; i<MAX_MUNICIPAL; i++) {
+		strcpy(nodes[i][0].cityName, "Muntinlupa");
+	}
 	
-	cMunicipality[0][3] = "Lourdes School";
-	cMunicipality[1][3] = "Shaw Boulevard";
-	cMunicipality[2][3] = "EDSA";
-	cMunicipality[3][3] = "Meralco";
-	cMunicipality[4][3] = "LaSalle GH";
+	// Taguig
+	strcpy(nodes[0][1].name, "SM Aura");
+	strcpy(nodes[1][1].name, "Bonifacio Global Center");
+	strcpy(nodes[2][1].name, "Mind Museum");
+	strcpy(nodes[3][1].name, "Enderun University");
+	strcpy(nodes[4][1].name, "Manila American Cemetery");
+		// Set city
+	for(i=0; i<MAX_MUNICIPAL; i++) {
+		strcpy(nodes[i][1].cityName, "Taguig");
+	}
 	
-	cMunicipality[0][4] = "SM Mall of Asia";
-	cMunicipality[1][4] = "De La Salle University";
-	cMunicipality[2][4] = "University of St Thomas";
-	cMunicipality[3][4] = "Rizal Park";
-	cMunicipality[4][4] = "Fort Santiago";
+	// Pasig
+	strcpy(nodes[0][2].name, "Hampton Gardens");
+	strcpy(nodes[1][2].name, "St. Paul College Pasig");
+	strcpy(nodes[2][2].name, "Estancia");
+	strcpy(nodes[3][2].name, "Medical City");
+	strcpy(nodes[4][2].name, "C5 Road");
+		// Set city
+	for(i=0; i<MAX_MUNICIPAL; i++) {
+		strcpy(nodes[i][2].cityName, "Pasig");
+	}
 	
-	cMunicipality[0][5] = "Ateneo De Manila";
-	cMunicipality[1][5] = "Memorial Circle";
-	cMunicipality[2][5] = "Eastwood";
-	cMunicipality[3][5] = "Cubao";
-	cMunicipality[4][5] = "Araneta Coliseum";
+	// Mandaluyong
+	strcpy(nodes[0][3].name, "Lourdes School of Mandaluyong");
+	strcpy(nodes[1][3].name, "Shaw Boulevard");
+	strcpy(nodes[2][3].name, "EDSA");
+	strcpy(nodes[3][3].name, "Meralco");
+	strcpy(nodes[4][3].name, "La Salle Greenhills");
+		// Set city
+	for(i=0; i<MAX_MUNICIPAL; i++) {
+		strcpy(nodes[i][3].cityName, "Mandaluyong");
+	}
 	
-	cMunicipality[0][6] = "Malabon Zoo";
-	cMunicipality[1][6] = "La Mesa Nature Reserve";
-	cMunicipality[2][6] = "Glorieta Park";
-	cMunicipality[3][6] = "San Bartolome Church";
-	cMunicipality[4][6] = "LRT Caloocan Mall";
+	// Manila
+	strcpy(nodes[0][4].name, "SM Mall of Asia");
+	strcpy(nodes[1][4].name, "De La Salle University");
+	strcpy(nodes[2][4].name, "University of Sto Thomas");
+	strcpy(nodes[3][4].name, "Rizal Park");
+	strcpy(nodes[4][4].name, "Fort Santiago");
+		// Set city
+	for(i=0; i<MAX_MUNICIPAL; i++) {
+		strcpy(nodes[i][4].cityName, "Manila");
+	}
 	
-	//Major City
-	printf("%s\t\t  %s\t\t   %s   %s\t       %s\t\t %s\t\t  %s", cCities[0], cCities[1], cCities[2], cCities[3], cCities[4], cCities[5], cCities[6]); printf("\n\n");
-	//Municipality
-	printf("%s\t\t %s\t  %s  %s %s\t %s %s\n", cMunicipality[0][0],cMunicipality[0][1],cMunicipality[0][2],cMunicipality[0][3],cMunicipality[0][4],cMunicipality[0][5],cMunicipality[0][6]);
-	printf("%s\t %s\t\t  %s  %s %s %s  %s\n", cMunicipality[1][0],cMunicipality[1][1],cMunicipality[1][2],cMunicipality[1][3],cMunicipality[1][4],cMunicipality[1][5],cMunicipality[1][6]);
-	printf("%s\t%s\t\t %s  %s\t\t  %s %s\t  %s\n", cMunicipality[2][0],cMunicipality[2][1],cMunicipality[2][2],cMunicipality[2][3],cMunicipality[2][4],cMunicipality[2][5],cMunicipality[2][6]);
-	printf("%s\t\t%s   %s  %s\t  %s\t\t %s\t\t  %s\n", cMunicipality[3][0],cMunicipality[3][1],cMunicipality[3][2],cMunicipality[3][3],cMunicipality[3][4],cMunicipality[3][5],cMunicipality[3][6]);
-	printf("%s     %s\t  %s  %s\t  %s\t\t %s %s\n", cMunicipality[4][0],cMunicipality[4][1],cMunicipality[4][2],cMunicipality[4][3],cMunicipality[4][4],cMunicipality[4][5],cMunicipality[4][6]);
+	// Quezon
+	strcpy(nodes[0][5].name, "Ateneo de Manila University");
+	strcpy(nodes[1][5].name, "Quezon Memorial Circle");
+	strcpy(nodes[2][5].name, "Eastwood");
+	strcpy(nodes[3][5].name, "Cubao");
+	strcpy(nodes[4][5].name, "Araneta Coliseum");
+		// Set city
+	for(i=0; i<MAX_MUNICIPAL; i++) {
+		strcpy(nodes[i][5].cityName, "Quezon");
+	}
+	
+	// Caloocan
+	strcpy(nodes[0][6].name, "Malabon Zoo");
+	strcpy(nodes[1][6].name, "La Mesa Nature Reserve");
+	strcpy(nodes[2][6].name, "Glorieta Park");
+	strcpy(nodes[3][6].name, "San Bartolome Church");
+	strcpy(nodes[4][6].name, "LRT Caloocan Mall");
+		// Set city
+	for(i=0; i<MAX_MUNICIPAL; i++) {
+		strcpy(nodes[i][6].cityName, "Caloocan");
+	}
+}
+
+void displaySPGrid(spNode nodes[][MAX_CITIES]) {
+	int i,j;
+	printf("===================================================================================================================================================================================================================\n");
+	printf("|%-29s|%-29s|%-29s|%-29s|%-29s|%-29s|%-29s|\n", nodes[0][0].cityName, nodes[0][1].cityName, nodes[0][2].cityName, nodes[0][3].cityName,
+		nodes[0][4].cityName, nodes[0][5].cityName, nodes[0][6].cityName);
+	printf("===================================================================================================================================================================================================================\n");
+	for(i=0; i<MAX_MUNICIPAL; i++) {
+		printf("|");
+		for(j=0; j<MAX_CITIES; j++) {
+			printf("%-29s|", nodes[i][j].name);
+		} 
+		printf("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+		printf("\n");
+	}	
+}
+
+
+/* *** MAIN FUNCTION ***/
+int main() {
+	spNode SPNodes[MAX_MUNICIPAL][MAX_CITIES];
+	setShortestPathNodes(SPNodes);
+	displaySPGrid(SPNodes);
+	
 	
 	return 0;
 }
