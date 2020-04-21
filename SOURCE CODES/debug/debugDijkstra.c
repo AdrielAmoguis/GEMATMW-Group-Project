d875ddf3f773b90c2bbd3b5c7d846ac05fe4444d
DLSUAdrielAmoguis <60635460+DLSUAdrielAmoguis@users.noreply.github.com>
2020-04-21 14:51:10 +0800

    [SP] Updated Dijkstra Debug Script

--- SOURCE CODES/debug/debugDijkstra.c
+++ SOURCE CODES/debug/debugDijkstra.c
@@ -8,14 +8,25 @@
 int main() {
 
     int i;
+    int start, end;
+    int mapSize;
 
     // Initializations
     spNode SPNodes[MAX_MUNICIPAL][MAX_CITIES];
 	spPath SPPaths[MAX_PATH];
 	setShortestPathNodes(SPNodes);
-    setShortestPathPaths(SPPaths, 1, 100);
 
-    spDijkstra spTest = dijkstra(0, 20, SPNodes, SPPaths);
+    printf("Enter maximum path distance: ");
+    scanf("%d", &mapSize);
+
+    setShortestPathPaths(SPPaths, 1, mapSize);
+
+    printf("Enter starting node ID: ");
+    scanf("%d", &start);
+    printf("Enter destination node ID: ");
+    scanf("%d", &end);
+
+    spDijkstra spTest = dijkstra(start, end, SPNodes, SPPaths);
 
     printf("Total Distance: %d\n", spTest.distance);
 
